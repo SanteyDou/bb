e6381feb5f5935fa4c014b60c8c21dde87bb9969
Oleksandr Kravets <santey.ua@gmail.com>
2018-01-25 13:43:08 +0200

    add laravel debugbar

--- config/app.php
+++ config/app.php
@@ -167,6 +167,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Barryvdh\Debugbar\ServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -225,6 +226,11 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        /*
+        * Application fasades
+        */
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
 
     ],
 
