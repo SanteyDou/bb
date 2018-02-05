b47904db39e4ee2190c6eaf3b89e2443cfad0ec1
Oleksandr Kravets <santey.ua@gmail.com>
2018-02-05 22:34:57 +0200

    set resources to local storages

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -80,7 +80,7 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
-    <script  src="http://code.jquery.com/jquery-3.3.1.min.js"  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="  crossorigin="anonymous"></script>
+    <script  src="{{ asset('js/jquery-3.3.1.min.js') }}"></script>
     <script defer src="{{ asset('js/font-awesome.js') }}"></script>
 
     @yield('scripts')
